1638749d4be4509ceb43802a0299ba4bd310f3b4
Day333 <109149132+Day333@users.noreply.github.com>
2026-01-28 16:17:20 +0800

    Update ETTm2.sh

--- scripts/TimeMosaic/ETTm2.sh
+++ scripts/TimeMosaic/ETTm2.sh
@@ -5,7 +5,7 @@ python -u run.py \
   --is_training 1 \
   --root_path ./dataset/ETT-small/ \
   --data_path ETTm2.csv \
-  --model_id ETTm2_96_96 \
+  --model_id ETTm2_320_96 \
   --model $model_name \
   --data ETTm2 \
   --features M \
@@ -28,7 +28,7 @@ python -u run.py \
   --is_training 1 \
   --root_path ./dataset/ETT-small/ \
   --data_path ETTm2.csv \
-  --model_id ETTm2_96_192 \
+  --model_id ETTm2_320_192 \
   --model $model_name \
   --data ETTm2 \
   --features M \
@@ -51,7 +51,7 @@ python -u run.py \
   --is_training 1 \
   --root_path ./dataset/ETT-small/ \
   --data_path ETTm2.csv \
-  --model_id ETTm2_96_336 \
+  --model_id ETTm2_320_336 \
   --model $model_name \
   --data ETTm2 \
   --features M \
@@ -73,7 +73,7 @@ python -u run.py \
   --is_training 1 \
   --root_path ./dataset/ETT-small/ \
   --data_path ETTm2.csv \
-  --model_id ETTm2_96_720 \
+  --model_id ETTm2_320_720 \
   --model $model_name \
   --data ETTm2 \
   --features M \
