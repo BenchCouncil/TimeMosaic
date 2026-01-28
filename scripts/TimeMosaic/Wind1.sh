5eae67a28b7581421235c5e69cbf2735b1e22933
Day333 <109149132+Day333@users.noreply.github.com>
2026-01-28 16:18:51 +0800

    Update Wind1.sh

--- scripts/TimeMosaic/Wind1.sh
+++ scripts/TimeMosaic/Wind1.sh
@@ -6,7 +6,7 @@ python -u run.py \
   --is_training 1 \
   --root_path ./dataset/wind/ \
   --data_path Location1.csv \
-  --model_id Wind1_96_96 \
+  --model_id Wind1_320_96 \
   --model $model_name \
   --data custom \
   --features M \
@@ -29,7 +29,7 @@ python -u run.py \
   --is_training 1 \
   --root_path ./dataset/wind/ \
   --data_path Location1.csv \
-  --model_id Wind1_96_192 \
+  --model_id Wind1_320_192 \
   --model $model_name \
   --data custom \
   --features M \
@@ -52,7 +52,7 @@ python -u run.py \
   --is_training 1 \
   --root_path ./dataset/wind/ \
   --data_path Location1.csv \
-  --model_id Wind1_96_336 \
+  --model_id Wind1_320_336 \
   --model $model_name \
   --data custom \
   --features M \
@@ -75,7 +75,7 @@ python -u run.py \
   --is_training 1 \
   --root_path ./dataset/wind/ \
   --data_path Location1.csv \
-  --model_id Wind1_96_720 \
+  --model_id Wind1_320_720 \
   --model $model_name \
   --data custom \
   --features M \
